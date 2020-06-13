3c5f7f79afb0a5fb8a73a883ba92be1af7f991eb
Artyom Myshlyaev <myshlyaev05@gmail.com>
2020-06-13 23:09:08 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Program1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Math;
+
+namespace Lab1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            double alpha;
+            Console.Write("Введите угол (в рад) и нажмите клавишу Enter: alpha = ");
+
+            while (true)
+            {
+                if (double.TryParse(Console.ReadLine(), out alpha)) break;
+                Console.WriteLine("Ошибка: нужно ввести число типа double!");
+            }
+
+            double z1 = Sin(PI / 2 + 3 * alpha) / (1 - Sin(3 * alpha - PI));
+            Console.WriteLine($"Значение функции z1 = {z1}");
+        }
+    }
+}
+
+// Examples :
+// 0.5 -> 0.0354
+// -1 -> -1.1527
+// 24 -> -0.7714
---
+++ Lab2_1/Program2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Math;
+
+namespace Lab2_1
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+
+            Console.Write("Введите аргумент x и нажмите клавишу Enter: x = ");
+
+            while (true)
+            {
+                if (double.TryParse(Console.ReadLine(), out x)) break;
+                Console.WriteLine("Ошибка: нужно ввести число типа double!");
+            }
+
+            if (x <= -6) y = 1;
+            else if (x <= -4) y = -0.5 * x - 2;
+            else if (x <= 0) y = Sqrt(4 - Pow(x + 2, 2));
+            else if (x <= 2) y = -Sqrt(1 - Pow(x - 1, 2));
+            else y = -x + 2;
+
+            Console.WriteLine("y({0}) = {1}", x, y);
+        }
+    }
+}
